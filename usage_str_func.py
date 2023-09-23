c0777cc8c511d208595fe35fb31da8c3edcdb524
Coder_Slytherin <89627685+SuhaniWonders@users.noreply.github.com>
2023-09-23 13:00:59 +0530

    Create usage_str_func.py

---
+++ usage_str_func.py
@@ -0,0 +1,19 @@
+#find() / replace()
+txt = "Hello, welcome to my world."
+
+change_find = txt.find("welcome")
+
+print(change_find)
+
+# len() 
+txt = "Hello, welcome to my world."
+length_str = len(txt)
+print(length_str)
+
+#upper() / lower()
+txt = "Hello, welcome to my world."
+uppercase_str = txt.upper()
+print(uppercase_str)
+
+# And other functions are provided in the README file, check them out !!
+
